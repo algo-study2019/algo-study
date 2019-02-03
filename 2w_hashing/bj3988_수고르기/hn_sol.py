6b86a7ece40da725e83450a8ea2b5944021915d3
LeoHNLee <leohnlee@gmail.com>
2019-02-03 11:03:02 +0900

    bj3988 not solved

--- 2w_hashing/bj3988_수고르기/hn_sol.py
+++ 2w_hashing/bj3988_수고르기/hn_sol.py
@@ -1,3 +1,13 @@
+from collections import deque
 n, k = input().split()
-v = input().split()
-localM = 0
+v = list(map(int, input().split()))
+v = deque(sorted(v))
+for _ in range(int(k)):
+    mx = v[1]-v[0]
+    mn = v[-1]-v[-2]
+    if mx>mn : v.popleft()
+    else : v.pop()
+m = v[-1]-v[0]
+for i, v_ in enumerate(list(v)[1:]):
+    if m>(v_-v[i]): m = v_-v[i]
+print(v[-1]-v[0]+m)
